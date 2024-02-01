6fea13d582dc8bc84fc402edd032044d9da53ea7
m.nechaev <m.nechaev@ritm.media>
2024-02-01 11:12:30 +0300

    Initial Commit

---
+++ NMAKickTask0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NMAKickTask0
+//
+//  Created by Нечаев Михаил on 01.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
